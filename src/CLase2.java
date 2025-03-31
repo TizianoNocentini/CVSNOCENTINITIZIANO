1db7f911bb50cb12c46515415ab85a71e3acb07a
Tiziano <Tiziano@DESKTOP-GOQ7CB1>
2025-04-01 00:25:15 +0200

    Segunda Confirmacion

---
+++ src/CLase2.java
@@ -0,0 +1,10 @@
+
+public class CLase2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("EN QUWE RAMA ESTAMOS?");
+        System.out.println("ESTAMOS EN LA RAMA DE PRODUCCION");
+    }
+    
+}
